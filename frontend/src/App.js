331f5fd7e6a62ea1aef65813b95be4819d21e2c9
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-07-12 12:18:33 -0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Visit <code>cats_code.js</code> and meoow!
         </p>
         <a
           className="App-link"
