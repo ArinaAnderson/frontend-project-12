a387e6c7a3b98628d9f2437e045b3e19a0475485
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-07-16 11:56:45 -0700

    STEP-2: add initial routing in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout.js';
+import Login from './components/Login.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Visit <code>cats_code.js</code> and meoow!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
-
+// <Route path="" element={} />
 export default App;
