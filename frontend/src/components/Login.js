b5b752a8d88321444d0037b5009e67626d6ee0b3
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-09-05 15:53:10 -0700

    set hmtl css for Login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
+import formik from 'formik';
 
 const Login = () => {
-  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const srcLink = location.state?.srcLink || '/';
+  const [errMsg, setErrMsg] = useState('');
+  const [loadingState, setLoadingState] = useState(false);
 
   return (
-    <div>
-      Login Page, meooow
-    </div>
+    <section className='login'>
+      <div className=' form-wrapper'>
+        <div className='form__img-box'>
+          <img />
+        </div>
+        <div>
+          <h1 className='login__title'>Войти</h1>
+          <form className='login__form form'>
+            <p
+              className={errMsg ? 'form__err-message' : 'offscreen'}
+              aria-live='assertive'
+            >
+              {errMsg}
+            </p>
+            <div className='form__input'>
+              <label htmlFor='username'>Ваш ник:</label>
+              <input
+                type='text'
+                name='username'
+                id='username'
+                autoComplete='off'
+                required
+              />
+            </div>
+            <p
+              className={errMsg ? 'form__err-message' : 'offscreen'}
+              aria-live='assertive'
+            >
+              {errMsg}
+            </p>
+            <div className='form__input'>
+              <label htmlFor='password'>Пароль:</label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                autoComplete='off'
+                required
+              />
+            </div>
+            <button className='form__btn-submit' disabled={loadingState} type="submit">Войти</button>
+          </form>
+        </div>
+      </div>
+      <div className='login__footer'>
+        <p>
+          Нет аккаунта? <span><Link to='/signup'>Регистрация</Link></span>
+        </p>
+      </div>
+    </section>
   );
 }
 
