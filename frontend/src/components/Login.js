bd0f271a2d55e0188f66d9c7837e0935de8fe5b5
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-09-24 20:39:08 -0700

    add formik to Login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,67 +1,115 @@
+import axios from '../api/axios.js';
+
 import React, { useRef, useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
-import formik from 'formik';
+import { useFormik } from 'formik';
+
+const LOGIN_URL = '/login';
 
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const srcLink = location.state?.srcLink || '/';
+  const from = location?.state?.from || '/';
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      // console.log(JSON.stringify(values, null, 2));
+      sendLoginRequest();
+    },
+  });
+
   const [errMsg, setErrMsg] = useState('');
   const [loadingState, setLoadingState] = useState(false);
 
+  const sendLoginRequest = async () => {
+    try {
+      const response = await axios({
+        method: 'post',
+        url: LOGIN_URL,
+        data: {
+          username: formik.values.username,
+          password: formik.values.password,
+        }
+      });
+
+      console.log(response.data);
+      // logIn();
+      // const stringifiedUserIdData = JSON.stringify({ token: response.data.token });
+      // console.log('stringifiedUserIdData', stringifiedUserIdData)
+      // localStorage.setItem('userId', stringifiedUserIdData);
+      // navigate(from.pathname);
+      navigate('/');
+    } catch(e) {
+      setErrMsg('the username or password is incorrect');
+      // inputEl.current.focus();
+    }
+  };
+
   return (
-    <section className='login'>
-      <div className=' form-wrapper'>
-        <div className='form__img-box'>
+    <section className="login">
+      <div className=" form-wrapper">
+        <div className="form__img-box">
           <img />
         </div>
         <div>
-          <h1 className='login__title'>Войти</h1>
-          <form className='login__form form'>
+          <h1 className="login__title">Войти</h1>
+          <form className="login__form form" onSubmit={formik.handleSubmit}>
             <p
               className={errMsg ? 'form__err-message' : 'offscreen'}
-              aria-live='assertive'
+              aria-live="assertive"
             >
               {errMsg}
             </p>
-            <div className='form__input'>
-              <label htmlFor='username'>Ваш ник:</label>
+            <div className="form__input">
+              <label htmlFor="username">Ваш ник:</label>
               <input
-                type='text'
-                name='username'
-                id='username'
-                autoComplete='off'
+                onChange={formik.handleChange}
+                value={formik.values.username}
+                type="text"
+                name="username"
+                id="username"
+                autoComplete="off"
                 required
               />
             </div>
-            <p
-              className={errMsg ? 'form__err-message' : 'offscreen'}
-              aria-live='assertive'
+
+            <div className="form__input">
+              <label htmlFor="password">Пароль:</label>
+              <input
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="off"
+                required
+              />
+            </div>
+            <button
+              className="form__btn-submit btn"
+              disabled={loadingState}
+              type="submit"
             >
-              {errMsg}
-            </p>
-            <div className='form__input'>
-              <label htmlFor='password'>Пароль:</label>
-              <input
-                type='password'
-                name='password'
-                id='password'
-                autoComplete='off'
-                required
-              />
-            </div>
-            <button className='form__btn-submit' disabled={loadingState} type="submit">Войти</button>
+              Войти
+            </button>
           </form>
         </div>
       </div>
-      <div className='login__footer'>
+      <div className="login__footer">
         <p>
-          Нет аккаунта? <span><Link to='/signup'>Регистрация</Link></span>
+          Нет аккаунта?{' '}
+          <span>
+            <Link to="/signup">Регистрация</Link>
+          </span>
         </p>
       </div>
     </section>
   );
-}
+};
 
 export default Login;
