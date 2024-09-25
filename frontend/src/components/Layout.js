0e096ab324b9e5335a9bae94c082e51a3aeabaad
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-09-24 20:30:28 -0700

    add Header component to Layout

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+
+import Header from './Header.js';
 
 const Layout = () => {
   return (
     <>
-      <div>
-        HEADER
-      </div>
+      <Header />
       <Outlet />
-      <div>
-        FOOTER
-      </div>
     </>
   );
 }
