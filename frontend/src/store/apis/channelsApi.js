218a5515e4bb68851e8d76c0994b82444297a848
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-10-07 15:01:47 -0700

    create RTK api for channels data

--- frontend/src/store/apis/channelsApi.js
+++ frontend/src/store/apis/channelsApi.js
@@ -2,42 +2,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const channelsApi = createApi({
   reducerPath: 'channels',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/v1' }),
-  prepareHeaders: (headers, { getState }) => { // (headers, { getState }) => {
-    // const token = getState().auth.token;
-    const token = localStorage.getItem('token');
-    console.log('TOKEN', token);
-    if (token) {
-      headers.set('Authorization', `Bearer ${token}`)
-    }
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api/v1',
+    prepareHeaders: (headers, { getState }) => {
+      console.log('AUTH TOKEN', getState().auth.token);
+      const token = getState().auth.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
 
-    return headers;
-  },
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getChannels: builder.query({
       providesTags: ['Channel'],
-      query: (user) => ({
+      query: () => ({
         url: '/channels',
       }),
     }),
-    /*
-        addAlbum: builder.mutation({
-            invalidatesTags: (result, error, user) => [{ type: 'UsersAlbums', id: user.userId }], // ['Album']
-            query: ({ title, userId }) => ({
-                url: '/albums',
-                body: { title, userId },
-                method: 'POST',
-            }),
-        }),
-        removeAlbum: builder.mutation({
-            invalidatesTags: (result, error, album) => [{ type: 'Album', id: album.id }], // ['Album']
-            query: (album) => ({
-                url: `/albums/${album.id}`,
-                method: 'DELETE',
-            }),
-        }),
-    */
-    }),
+  }),
 });
 
 export const { useGetChannelsQuery } = channelsApi;
