b2bfb4ab26d7ca79846de7c99900f07972b58409
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-10-07 15:09:10 -0700

    add auth reducer to the store (global state)

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { channelsApi } from './apis/channelsApi.js';
+import authReducer from './slices/authSlice.js';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
