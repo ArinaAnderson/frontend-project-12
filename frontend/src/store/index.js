24fc9f3477e97c683de7851e3554ea9f509ebb2e
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-10-14 16:16:25 -0700

    add messages reducer to the global store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { channelsApi } from './apis/channelsApi.js';
+import { messagesApi } from './apis/messagesApi.js';
 import authReducer from './slices/authSlice.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .concat(channelsApi.middleware),
+    .concat(channelsApi.middleware)
+    .concat(messagesApi.middleware),
 });
 
 setupListeners(store.dispatch);
