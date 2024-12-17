5c70832ed2aa8708ca62543c87b687cf350e26ad
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-12-16 22:48:09 -0800

    fix css for sign up err messages

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -11,27 +11,10 @@ import updateLocalStorage from '../utils/localStorage.js';
 import { ROUTES, API_ROUTES } from '../utils/router.js';
 
 const Signup = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
-  const [errMsg, setErrMsg] = useState('');
+  // const [errMsg, setErrMsg] = useState('');
   const [err, setErr] = useState(null);
-
-  const generateErrorMessage = (error) => {
-    if (error === null) {
-      return '';
-    }
-
-    const errorMessageText = error?.response?.status === 409
-      ? t('form.signup.errors.err409')
-      : t('errors.noNetwork');
-
-    return errorMessageText;
-  };
-
-  i18n.on('languageChanged', () => {
-    const errMessage = generateErrorMessage(err);
-    setErrMsg(errMessage);
-  });
 
   const dispatch = useDispatch();
 
@@ -62,7 +45,8 @@ const Signup = () => {
 
   const sendSignupRequest = async (formikInst) => {
     formikInst.setSubmitting(true);
-    setErrMsg('');
+    setErr(null);
+    // setErrMsg('');
     try {
       const response = await axios({
         method: 'post',
@@ -78,11 +62,6 @@ const Signup = () => {
 
       navigate('/');
     } catch (e) {
-      const errorMessageText = e?.response?.status === 409
-        ? t('form.signup.errors.err409')
-        : t('errors.noNetwork');
-
-      setErrMsg(errorMessageText);
       setErr(e);
       inputRef.current.select();
     } finally {
@@ -111,101 +90,109 @@ const Signup = () => {
   return (
     <section className="signup">
       <div className="form-wrapper">
+        <p
+          className={err ? 'form__err-message--main' : 'offscreen'}
+          aria-live="assertive"
+        >
+          {
+            err?.response?.status === 409
+              ? t('form.signup.errors.err409')
+              : t('errors.noNetwork')
+          }
+        </p>
         <div>
           <h1 className="login__title">{t('form.signup.headline')}</h1>
           <form className="login__form form" onSubmit={formik.handleSubmit}>
-            <p
-              className={errMsg ? 'form__err-message form__err-message--main' : 'offscreen'}
-              aria-live="assertive"
-            >
-              {errMsg}
-            </p>
-            <p
-              id="usernameErrNote"
-              className={
-                formik.errors.username && formik.touched.username ? 'form__err-message' : 'offscreen'
-              }
-            >
-              {formik.errors.username}
-            </p>
-            <div className="form__input-box">
-              <label className="form__label" htmlFor="username">
-                {t('form.signup.labels.username')}
-                :
-              </label>
-              <input
-                className="form__input"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.username}
-                type="text"
-                name="username"
-                id="username"
-                autoComplete="off"
-                ref={inputRef}
-                required
-                aria-invalid={formik.errors.username ? 'true' : 'false'}
-                aria-describedby="usernameErrNote"
-              />
-            </div>
-
-            <p
-              id="passwordErrNote"
-              className={
-                formik.errors.password && formik.touched.password ? 'form__err-message' : 'offscreen'
-              }
-            >
-              {formik.errors.password}
-            </p>
-            <div className="form__input-box">
-              <label className="form__label" htmlFor="password">
-                {t('form.signup.labels.password')}
-                :
-              </label>
-              <input
-                className="form__input"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.password}
-                type="password"
-                name="password"
-                id="password"
-                autoComplete="off"
-                required
-                aria-invalid={formik.errors.password ? 'true' : 'false'}
-                aria-describedby="passwordErrNote"
-              />
-            </div>
-
-            <p
-              id="confirmPasswordErrNote"
-              className={
-                formik.errors.confirmPassword && formik.touched.confirmPassword ? 'form__err-message' : 'offscreen'
-              }
-            >
-              {formik.errors.confirmPassword}
-            </p>
-            <div className="form__input-box">
-              <label
-                className="form__label"
-                htmlFor="confirmPasswordß"
+            <div className="form__unit">
+              <p
+                id="usernameErrNote"
+                className={
+                  formik.errors.username && formik.touched.username ? 'form__err-message' : 'offscreen'
+                }
               >
-                {t('form.signup.labels.confirmPassword')}
-                :
-              </label>
-              <input
-                className="form__input"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.confirmPassword}
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                autoComplete="off"
-                required
-                aria-invalid={formik.errors.confirmPassword ? 'true' : 'false'}
-                aria-describedby="confirmPasswordErrNote"
-              />
+                {formik.errors.username}
+              </p>
+              <div className="form__input-box">
+                <label className="form__label" htmlFor="username">
+                  {t('form.signup.labels.username')}
+                  :
+                </label>
+                <input
+                  className="form__input"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  ref={inputRef}
+                  required
+                  aria-invalid={formik.errors.username ? 'true' : 'false'}
+                  aria-describedby="usernameErrNote"
+                />
+              </div>
+            </div>
+            <div className="form__unit">
+              <p
+                id="passwordErrNote"
+                className={
+                  formik.errors.password && formik.touched.password ? 'form__err-message' : 'offscreen'
+                }
+              >
+                {formik.errors.password}
+              </p>
+              <div className="form__input-box">
+                <label className="form__label" htmlFor="password">
+                  {t('form.signup.labels.password')}
+                  :
+                </label>
+                <input
+                  className="form__input"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  required
+                  aria-invalid={formik.errors.password ? 'true' : 'false'}
+                  aria-describedby="passwordErrNote"
+                />
+              </div>
+            </div>
+            <div className="form__unit">
+              <p
+                id="confirmPasswordErrNote"
+                className={
+                  formik.errors.confirmPassword && formik.touched.confirmPassword ? 'form__err-message' : 'offscreen'
+                }
+              >
+                {formik.errors.confirmPassword}
+              </p>
+              <div className="form__input-box">
+                <label
+                  className="form__label"
+                  htmlFor="confirmPasswordß"
+                >
+                  {t('form.signup.labels.confirmPassword')}
+                  :
+                </label>
+                <input
+                  className="form__input"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.confirmPassword}
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  autoComplete="off"
+                  required
+                  aria-invalid={formik.errors.confirmPassword ? 'true' : 'false'}
+                  aria-describedby="confirmPasswordErrNote"
+                />
+              </div>
             </div>
             <button
               className="form__btn-submit  bttn"
